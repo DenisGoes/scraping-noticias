c7219497eb60e9d4f8cb27052d68c520a56d8364
Denis <d.goesdev@gmail.com>
2026-08-20 00:33:26 -0300

    scraper_nt.yml editado para testes

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
 from backend.api.routes.webhook_telegram import webhook_router
 
-
-
 app = FastAPI()
 
 
 @app.get("/")
 def healthz_check():
-    return{"status": "ok"}
+    return {"status": "ok"}
 
 
 app.include_router(webhook_router)
