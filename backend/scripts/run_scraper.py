79fae046ded8d7e2fb9110f38dfbfaf64819199f
Denis <d.goesdev@gmail.com>
2026-08-21 12:12:24 -0300

    Atualizado script scraper

---
+++ backend/scripts/run_scraper.py
@@ -0,0 +1,12 @@
+# from backend.scrapers.tecnologia.tecnoblog.scraper import run_scraper_tec
+# from backend.scrapers.tecnologia.bleepingcomputer.scraper import run_scraper_bleeping
+from backend.scrapers.tecnologia.hackernews.scraper import run_scraper_hacker
+
+def main():
+    # run_scraper_tec()
+    # run_scraper_bleeping()
+    run_scraper_hacker()
+
+
+if __name__ == "__main__":
+    main()
--- backend/scripts/run_tecnoblog.py
+++
@@ -1,9 +0,0 @@
-from backend.scrapers.tecnologia.tecnoblog.scraper import run_scraper
-
-
-def main():
-    run_scraper()
-
-
-if __name__ == "__main__":
-    main()
